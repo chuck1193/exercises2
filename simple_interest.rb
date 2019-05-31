7f38e04fd55ba0037560abbd455a98bfd4f2a41f
chuck1193 <>
2019-05-31 13:19:15 -0500

    Computing Simple Interest

---
+++ simple_interest.rb
@@ -0,0 +1,14 @@
+puts "Enter the principal: "
+principal = gets.chomp.to_f
+
+puts "Enter the rate of interest: "
+rate_of_interest = gets.chomp.to_f
+
+puts "Enter the number of years: "
+years = gets.chomp.to_f
+
+rate = rate_of_interest / 100
+
+end_amount = principal * (1 + (rate * years))
+
+puts "After #{years} years at #{rate_of_interest}%, the investment will be worth #{end_amount}."
