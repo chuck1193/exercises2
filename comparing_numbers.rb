0fbfc5b5840756bde972fff081a221681646ff9a
chuck1193 <>
2019-06-28 15:59:49 -0500

    Troubleshooting Car Issues

--- comparing_numbers.rb
+++ comparing_numbers.rb
@@ -1,37 +1,41 @@
 def comparing_numbers
-  puts "enter the first number: "
-  first_number = gets.chomp.to_i
-
-  puts "Enter the second number: "
-  second_number = gets.chomp.to_i
-
-  puts "Enter the third number: "
-  third_number = gets.chomp.to_i
 
   array = []
   i = 0
-  array << first_number
-  array << second_number
-  array << third_number
+  n = array.length
 
-  if first_number == second_number || first_number == third_number || second_number == third_number
+  10.times  do
+    puts "enter a number: "
+    number = gets.chomp.to_i
+
+    array << number
+
+    puts "Do you want to cotinue adding numbers? "
+    answer = gets.chomp
+
+    if answer == 'yes'
+      next
+    elsif answer == 'no'
+      break
+    end
+  end
+
+  if array[i] == array[i + 1] || array[i] == array[i - 1] || array[i + 1] == array[i - 1]
     exit
   else
-    n = array.length
-
     loop do
       swapped = false
 
       (n - 1).times do |i|
-        if array[i] < array[i + 1]
+        if array[i] > array[i + 1]
           array[i], array[i + 1] = array[i + 1], array[i]
           swapped = true
         end
       end
       break if not swapped
     end
+    puts "The largest number is: #{array}"
   end
-  puts "The largest number is: #{array[i]}"
 end
 
 comparing_numbers
