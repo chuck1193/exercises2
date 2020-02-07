153b23a9695d193e732bd4e277296d9dc8db1bf6
chuck1193 <>
2020-02-06 21:46:43 -0600

    Used input from another file and filitered through that data. (Super dope.)

--- filtering_values.rb
+++ filtering_values.rb
@@ -1,14 +1,24 @@
 def filtering_values
-  puts "Enter a list of numbers, separated by spaces: "
-  number_list = gets.split.map(&:to_i)
-
+  # puts "Enter a list of numbers, separated by spaces: "
+  # number_list = gets.split.map(&:to_i)
+  
   even_number_arr = []
-  number_list.each do |number|
-    if  number % 2 == 0
-      even_number_arr << number
+  File.open("number_arrr.txt").each do |line|
+    arr = line.to_s.chars.map(&:to_i)
+    arr.each do |number|
+      if number % 2 == 0
+        even_number_arr << number
+      end
     end
   end
   p even_number_arr
+  # p even_number_arr
+  # number_list.each do |number|
+  #   if  number % 2 == 0
+  #     even_number_arr << number
+  #   end
+  # end
+  # p even_number_arr
 end
 
 filtering_values
